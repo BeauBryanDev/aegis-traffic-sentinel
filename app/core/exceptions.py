45fce5718dc3ae1e9b4d50c6a9ab033d1a4eef70
BeauBryanDev <bryan.beau.186@gmail.com>
2026-06-20 12:15:01 -0500

    set init backend file and dir structure

---
+++ app/core/exceptions.py
@@ -0,0 +1,11 @@
+class VideoValidationError(Exception):
+    pass
+
+class InferenceError(Exception):
+    pass
+
+class ModelNotLoadedError(Exception):
+    pass
+
+class StreamDisconnectedError(Exception):
+    pass
